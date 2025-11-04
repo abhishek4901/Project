e5e16960ae3bed81f3c2118cf3780738259936a9
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-11-05 01:50:38 +0530

    Fix import statement formatting in EmployeeManagementApp

--- Employee management/src/com/employee/app/EmployeeManagementApp.java
+++ Employee management/src/com/employee/app/EmployeeManagementApp.java
@@ -3,7 +3,7 @@ package com.employee.app;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.sql.SQLException; 
-import java.util.regex.Pattern;
+import java.util.regex.Pattern; 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder; 
